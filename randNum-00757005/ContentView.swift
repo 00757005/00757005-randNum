44a0eec2fc7537f9933c5a384e74dc1d2299db4d
andy <andy870622@gmail.com>
2021-01-18 22:21:58 +0800

    Initial Commit

---
+++ randNum-00757005/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  randNum-00757005
+//
+//  Created by User15 on 2021/1/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ randNum-00757005/randNum_00757005App.swift
@@ -0,0 +1,17 @@
+//
+//  randNum_00757005App.swift
+//  randNum-00757005
+//
+//  Created by User15 on 2021/1/18.
+//
+
+import SwiftUI
+
+@main
+struct randNum_00757005App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
